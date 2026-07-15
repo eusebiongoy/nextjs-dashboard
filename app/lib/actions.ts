4ae54e2ee43c0d5766ae5b2116eb4ebc2beeb8f1
eusebiongoy <banzaeusebio2@gmail.com>
2026-07-15 02:41:19 +0200

    fixing chapter 13

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -11,9 +11,15 @@ const sql = postgres(process.env.POSTGRES_URL!, {
 
 const FormSchema = z.object({
   id: z.string(),
-  customerId: z.string(),
-  amount: z.coerce.number(),
-  status: z.enum(['pending', 'paid']),
+  customerId: z.string({
+    invalid_type_error: 'Please select a customer.',
+  }),
+  amount: z.coerce
+    .number()
+    .gt(0, { message: 'Please enter an amount greater than $0.' }),
+  status: z.enum(['pending', 'paid'], {
+    invalid_type_error: 'Please select an invoice status.',
+  }),
   date: z.string(),
 });
 
@@ -27,17 +33,37 @@ const UpdateInvoice = FormSchema.omit({
   date: true,
 });
 
+export type State = {
+  errors?: {
+    customerId?: string[];
+    amount?: string[];
+    status?: string[];
+  };
+  message?: string | null;
+};
 
-export async function createInvoice(formData: FormData) {
-  const {
-    customerId,
-    amount,
-    status,
-  } = CreateInvoice.parse({
+export async function createInvoice(
+  prevState: State,
+  formData: FormData,
+) {
+  const validatedFields = CreateInvoice.safeParse({
     customerId: formData.get('customerId'),
     amount: formData.get('amount'),
     status: formData.get('status'),
   });
+
+  if (!validatedFields.success) {
+    return {
+      errors: validatedFields.error.flatten().fieldErrors,
+      message: 'Missing Fields. Failed to Create Invoice.',
+    };
+  }
+
+  const {
+    customerId,
+    amount,
+    status,
+  } = validatedFields.data;
 
   const amountInCents = amount * 100;
 
@@ -62,17 +88,19 @@ export async function createInvoice(formData: FormData) {
     `;
   } catch (error) {
     console.error('Database Error:', error);
-    throw new Error('Failed to create invoice.');
+
+    return {
+      message: 'Database Error: Failed to Create Invoice.',
+    };
   }
 
   revalidatePath('/dashboard/invoices');
   redirect('/dashboard/invoices');
 }
 
-
 export async function updateInvoice(
   id: string,
-  formData: FormData
+  formData: FormData,
 ) {
   const {
     customerId,
